6897aee13eb1f96fbb4cac853f80a0ab9bb41bd7
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2022-01-06 20:36:45 +0800

    fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,24 +1,5 @@
 # frozen_string_literal: true
 
 require_relative 'application'
-require_relative 'rollbar'
 
 Rails.application.initialize!
-
-notify = lambda do |e|
-  Rollbar.with_config(use_async: false) do
-    Rollbar.error(e)
-  end
-rescue StandardError
-  Rails.logger.error 'Synchronous Rollbar notification failed.  Sending async to preserve info'
-  Rollbar.error(e)
-end
-
-# rubocop:disable Lint/RescueException
-begin
-  Rails.application.initialize!
-rescue Exception => e
-  notify.call(e)
-  raise
-end
-# rubocop:enable Lint/RescueException
