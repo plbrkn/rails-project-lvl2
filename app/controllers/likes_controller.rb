5d41a247177859e0863dbe5c0506b2313724bc74
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2022-02-05 14:42:13 +0800

    fix likes contoller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,18 +4,17 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    return if post.likes.find_by(user: current_user)
-
-    post.likes.create(user: current_user)
+    unless post.likes.find_by(user: current_user)
+      post.likes.create(user: current_user)
+    end
     redirect_to post
   end
 
   def destroy
     like = post.likes.find_by(user: current_user)
-    if like
-      like.destroy
-      redirect_to post
-    end
+    like&.destroy
+
+    redirect_to post
   end
 
   private
