1f5e1e303fc829f1927f62b5452e6159a6b6b5ae
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2022-02-06 21:15:17 +0800

    changed name links in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@
 
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'creator_id', inverse_of: :creator, dependent: :destroy
-  has_many :post_comments, dependent: :destroy
-  has_many :post_likes, dependent: :destroy
+  has_many :comments, class_name: 'PostComment', dependent: :destroy
+  has_many :likes, class_name: 'PostLike', dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
