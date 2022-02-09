665238ce326fa8ebe8bc631e4fd2677cbf545194
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2022-02-09 16:13:38 +0800

    add fortawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "../stylesheets/application.scss";
 import "channels";
+import "@fortawesome/fontawesome-free/css/all";
 
 Rails.start();
 Turbolinks.start();
